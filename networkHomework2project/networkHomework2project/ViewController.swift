4a91229bb43b6af3907d8dc39798e50cdb956465
mi1212 <mc_gorba4ev@mail.ru>
2023-02-27 12:51:49 +0300

    added project

---
+++ networkHomework2project/networkHomework2project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  networkHomework2project
+//
+//  Created by Mikhail Chuparnov on 27.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
